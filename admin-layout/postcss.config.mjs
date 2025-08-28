10748bae5184ba1bbee35834bf27dccdcd424532
tam986 <thanhtam1532@gmail.com>
2025-08-28 19:33:21 +0700

    start 28/8/2025

---
+++ admin-layout/postcss.config.mjs
@@ -0,0 +1,12 @@
+// const config = {
+//   plugins: ["@tailwindcss/postcss"],
+// };
+
+// export default config;
+/** @type {import('postcss-load-config').Config} */
+const config = {
+  plugins: {
+    "@tailwindcss/postcss": {},
+  },
+};
+export default config;
